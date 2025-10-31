0ca936c420504935aa091d6d562c4b3f8008ff3c
hngoy <ngoy.h@northeastern.edu>
2025-10-31 11:01:44 -0700

    Added RGB Light Functionality
    Will re-add music functionality and threading later on when I am not having a stroke over finding the fourier transform for this monstrosity

--- main.py
+++ main.py
@@ -2,27 +2,34 @@
 from picozero import *
 from time import sleep
 
-# Imports multithreading capabilities if you don't know what that is don't touch
-import threading
-
 """
 The different sections in order are:
 - RGB light
 - Potentiometer and fan
 - Lights with buttons
-- Speaker hooked up to button
 """
 
 # Ports for the RGB Light
-RGB_red = 21
-RGB_blue = 20
-RGB_green = 19
+RGB_red = 5
+RGB_green = 8
+RGB_blue = 9
+
+# RGB Ports for rgb object
+rgb = RGBLED(RGB_red, RGB_green, RGB_blue)
+
+# Color Values for RGB Light
+valuesRGB = [0,0,0]
+
+# Buttons for RGB Light
+ButtonR = Button(2)
+ButtonG = Button(3)
+ButtonB = Button(4)
 
 # Ports for controlling Fan backwards and forwards
 FanMotor = Motor(0,1,False)
 
 # Port for Potentiometer for fan
-Potential = Potentiometer(16)
+# Potential = Potentiometer(16)
 
 # Ports for controlling lights
 Light1 = LED(6)
@@ -34,10 +41,26 @@ Button1 = Button(7)
 Button2 = Button(11)
 Button3 = Button(15)
 
-# Speaker is configured in a separate file due to complexity
-# Speaker Button and pin
-SpeakButton = Button(29)
-
-# Update loop, do not touch as 90% of it is related to multithreading for audio processing capabilities
 while True:
-    
+    # Set and Reset Red Value
+    if ButtonR.is_pressed:
+        valuesRGB[0] = 255
+    if ButtonR.is_open:
+        valuesRGB[0] = 0
+
+    # Set and Reset Green Value
+    if ButtonG.is_pressed:
+        valuesRGB[1] = 255
+    if ButtonG.is_open:
+        valuesRGB[1] = 0
+
+    # Set and Reset Blue Value
+    if ButtonB.is_pressed:
+        valuesRGB[2] = 255
+    if ButtonB.is_open:
+        valuesRGB[2] = 0
+
+    # Creates Output
+    rgb.color = valuesRGB
+    sleep(0.1)
+
